80e1c64dad949361d9727972c37f4889ca5d275c
aishaschreiber <aisha.schreiber36@gmail.com>
2023-09-13 11:58:57 +1000

    basic formulation

--- Layout.py
+++ Layout.py
@@ -4,17 +4,23 @@ import access_data_edit
 GET = access_data_edit.get_data_sets()
 #SETS   
 # TF = range(76+1) # Set of temporary facility locations (aka 'I' in the paper)
-TF = GET['TF']
+# TF = GET['TF']
+TF = range(76+1)
 # PF = range(34+1) # Set of permanent facility (PF) locations J (aka 'J' in the paper)
-PF = GET['PF']
+# PF = GET['PF']
+PF = range(34+1)
 # K = range(2+1) # Set of service coverage windows
-K = GET['K']
+# K = GET['K']
+K = range(2+1)
 # T = range(1+1) # Set of PF sizes
-T = GET['T']
+# T = GET['T']
+T = range(1+1)
 # S = range(1+1) # Set of scenarios
-S = GET['S']
+# S = GET['S']
+S = range(1+1)
 # L = range(2+1) # Set of items
-L = GET['L']
+# L = GET['L']
+L = range(2+1)
 # dictionary J_ik[(i,k)] Set of PFs that can service TF i within SCW k  ( PFik ⊆ PFik', for k<k' )
 PF_ik = GET['PF_ik']
 
@@ -25,7 +31,7 @@ TF_m = GET['TF_m']
 
 #PARAMETERS
 # dictoinary D_mls[(m,l,s)] Demand of point m in Ms for item l in L under scenario s in S 
-D_mls = GET['D_mls']
+D_lms = GET['D_mls']
 # dictoinary R_lk[(l,k)] Proportion of demand for item l to be satisfied within service coverage window k in K
 R_lk = GET['R_lk']
 # dictoinary KP_t[t] Capacity of a PF of size t in T
@@ -38,35 +44,34 @@ u_l = GET['u_l']
 h_l = GET['h_l']
 # dictoinary c_jt[(j,t)] Fixed cost of PF at location j of size t
 c_jt = GET['c_jt']
-# dictoinary delta_im[(i,m)] Distance between TF i in TF and demand point m in M(s)
+# dictoinary delta_im[(i,m)] Distance between TF i and demand point m in M_s(s)
 delta_im = GET['delta_im']
 
 # dictoinary C_s[s]
 C_s = GET['C_s']
 
 
-m = Model("Attempt1")
+LP = Model("Attempt1")
 
 #VARIABLES
 # 1, if demand point m in assigned to TF i for scenario s
-# X = {(i,m,s): m.addVar(vtype=GRB.BINARY) for s in S for i in TF for m in M_s[s]}
-X = {(i,m,s): m.addVar(vtype=GRB.BINARY) for s in S for i in TF}
+X = {(i,s,m): LP.addVar(vtype=GRB.BINARY) for s in S for i in TF for m in M_s[s]}
 
 # 1, if TF i is opened under scenario s
-Y = {(i,s): m.addVar(vtype=GRB.BINARY) for s in S for i in TF}
+Y = {(i,s): LP.addVar(vtype=GRB.BINARY) for s in S for i in TF}
 
 # amount of item l transported from PJ j to TF i under scenario s
-F = {(j,i,l,s): m.addVar() for s in S for j in PF for i in TF for l in L}
+F = {(j,i,l,s): LP.addVar() for s in S for j in PF for i in TF for l in L}
 
 # 1, if PF j of size t is opened
-Z = {(j,t): m.addVar(vtype=GRB.BINARY) for j in PF for t in T}
+Z = {(j,t): LP.addVar(vtype=GRB.BINARY) for j in PF for t in T}
 
 # prepositioned invenctory amount of item l at PF j
-I = {(j,l): m.addVar() for j in PF for l in L}
+I = {(j,l): LP.addVar() for j in PF for l in L}
 
 
 #OBJECTIVE
-m.setObjective(
+LP.setObjective(
     quicksum(c_jt[(j,t)][0]*Z[j,t] for j in PF for t in T)+
     quicksum(h_l[l][0]*I[j,l] for j in PF for l in L),
     GRB.MINIMIZE)
@@ -74,50 +79,48 @@ m.setObjective(
 # # CONSTRAINTS
 
 
-# # Each demand point assigned to one TF
-# TWO = {(s,m):
-#         m.addConstr(quicksum(X[s,i,m] for i in TF_m[m]) == 1)
-#             for s in S for m in M_s[s]}
+# Each demand point assigned to one TF
+TWO = {(s,m):
+        LP.addConstr(quicksum(X[i,s,m] for i in TF_m[m]) == 1)
+            for s in S for m in M_s[s]}
 
-# # Demand points only assigned to TFs that are open
-# THREE = {(i,m,s):
-#         m.addConstr(X[i,m,s] <= Y[i,s])
-#         for i in TF for m in M_s[s] for s in S}
+# Demand points only assigned to TFs that are open
+THREE = {(i,m,s):
+        LP.addConstr(X[i,s,m] <= Y[i,s])
+        for i in TF for s in S for m in M_s[s]}
 
-# # TF capacity is not violated
-# # demand points that are opened * capacity consumption  of item l , is <= capacity of TF i 
-# FOUR = {(i,s):
-#         m.addConstr(quicksum(u_l[l][0]*quicksum(D_mls[(m,l,s)][0]*X[i,m,s] for m in M_s[s]) for l in L) <= KT_i[i][0]*Y[i,s])
-#             for i in TF for s in S}
+# TF capacity is not violated
+# demand points that are opened * capacity consumption  of item l , is <= capacity of TF i 
+FOUR = {(i,s):
+        LP.addConstr(quicksum(u_l[l][0]*quicksum(D_lms[(l,m,s)][0]*X[i,s,m] for m in M_s[s] if (l,m,s) in D_lms) for l in L) <= KT_i[i][0]*Y[i,s])
+            for i in TF for s in S}
 
-# # Demand points are assigned to the closest TF 
-# # ----??????????
-# FIVE = {(i,m,s):
-#         m.addConstr(quicksum(X[n,m,s] >= Y[i,s] for n in TF_m[m] and delta_im[(n,m)][0] <= delta_im[(i,m)][0]))
-#         for s in S for m in M_s[s] for i in TF_m[m]}
+# Demand points are assigned to the closest TF 
+FIVE = {(i,s,m):
+        LP.addConstr(quicksum(X[n,s,m] for n in TF_m[m] 
+        if delta_im[(n,m)][0] <= delta_im[(i,m)][0]) >= Y[i,s])
+        for s in S for m in M_s[s] for i in TF_m[m]}
 
 # Amount of item l transported from PF to TF in scenario S, is >= proportion of demand to be satisfied in SCW k 
-# SIX = {(s,i,k,l):
-#         m.addConstr(quicksum(F[j,i,l,s] for j in PF_ik[(i,k)]) >= R_lk[(l,k)][0]*quicksum(D_mls[(m,l,s)][0]*X[i,m,s] for m in M_s[s]))
-#         for s in S for i in TF for k in K for l in L}
-    
-# SIX = {(s,i,k,l): 
-#         m.addConstr(quicksum(F[j,i,l,s] for j in PF_ik[(i,k)]) >= R_lk[(l,k)][0]*quicksum(D_mls[(M_s[s],l,s)][0]*X[i,M_s[s],s] for M_s[s] in M_s[s]))
-#         for s in S for i in TF for k in K for l in L}
+SIX = {(s,i,k,l):
+        LP.addConstr(quicksum(F[j,i,l,s] for j in PF_ik[(i,k)])
+        >= R_lk[(l,k)][0]*quicksum(D_lms[(l,m,s)][0]*X[i,s,m] for m in M_s[s] if (l,m,s) in D_lms))
+        for s in S for i in TF for k in K for l in L}
+
 
 # capacity consumption of item l * amount of prepositioned inventory is <= capacity of TF
 SEVEN = {j:
-          m.addConstr(quicksum(u_l[l][0]*I[j,l] for l in L) <= quicksum(KP_t[t][0]*Z[j,t] for t in T))
+          LP.addConstr(quicksum(u_l[l][0]*I[j,l] for l in L) <= quicksum(KP_t[t][0]*Z[j,t] for t in T))
                       for j in PF}
 
 # the amount of items that can be shipped from a PF under each scenario is limited by the prepositioned amount
 EIGHT = {(j,l,s):
-          m.addConstr(quicksum(F[j,i,l,s] for i in TF) <= I[j,l])
+          LP.addConstr(quicksum(F[j,i,l,s] for i in TF) <= I[j,l])
           for j in PF for l in L for s in S}
 
 # force the model to open at most one size of PF at each candidate location
 NINE = {j:
-        m.addConstr(quicksum(Z[j,t] for t in T) <= 1)
+        LP.addConstr(quicksum(Z[j,t] for t in T) <= 1)
         for j in PF}
 
 # # TEN: X is a binary varialbe
@@ -126,5 +129,5 @@ NINE = {j:
 # # THIRTEEN: Z is a binary variable
 # # FOURTEEN: I >= 0 variable
 
-# m.optimize()
+LP.optimize()
 
