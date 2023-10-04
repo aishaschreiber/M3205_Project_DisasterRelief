c1039f6c4863413f4ae7884e0b4cf688c9e9b4f6
aishaschreiber <aisha.schreiber36@gmail.com>
2023-10-04 20:13:45 +1000

    Update LBBD_Aishas_attempt.py

--- LBBD_Aishas_attempt.py
+++ LBBD_Aishas_attempt.py
@@ -1,4 +1,5 @@
 from gurobipy import *
+import access_data_edit
 import sys
 import os
 
@@ -117,6 +118,7 @@ LIPMP.optimize()
 
 CSP = Model("Clustering Subproblem") 
 
+s = 0
 # CSP Variables
 X = {(i,m): CSP.addVar(vtype=GRB.BINARY) for i in TF for m in M_s[s]}
 
@@ -128,179 +130,179 @@ EIGHTEEN = {m:
 
 NINETEEN = {(m,i):
             CSP.addConstr(X[i,m] <= Y[i,s].x) 
-            for m in M_s[s] for i in TF_m}
+            for m in M_s[s] for i in TF_m[m]}
 
 TWENTY = {i:
           CSP.addConstr(quicksum(u_l[l]*D_sml[(s,m,l)]*X[i,m] for l in L for m in M_s[s]) \
                         <= KT_i[i] * Y[i,s].x) 
-            for i in TF_m[m]}
+            for i in TF}
 
-# CSP.optimize()
+CSP.optimize()
 
 ##################################
 ##### Reduced CSP SUB-PROBLEM ####
 ##################################
 #It is equvalent to CSP after fixing the values of the X variables for which a unique closest TF exist.
 
-# Dictionary of demand points with multiple closest TFs
-Md_s = {}
-# Dictionary of TFs that are the same distance from a demand point m (alternatives)
-TFd_sm = {}
-# For each scenario s in S
-for s in S:
-    multiples = []
-    # For each demand point m in M_s[s]
-    for m in M_s[s]:
-        # Find the closest distance
-        closest_distance = 1000000
-        # For each TF i in I_m[m]
-        for i in TF_m[m]:
-            if delta_im[(i,m)] < closest_distance:
-                closest_distance = delta_im[(i,m)]
-        # Find the TFs that are that distance away
-        closest_TFs = []
-        # For each TF i in I_m[m]
-        for i in TF_m[m]:
-            if delta_im[(i,m)] == closest_distance:
-                closest_TFs.append(i)
-        # Add to dictionary
-        TFd_sm[(s,m)] = closest_TFs
-        if len(closest_TFs) > 1:
-            multiples.append(m)
-    Md_s[s] = multiples
+# # Dictionary of demand points with multiple closest TFs
+# Md_s = {}
+# # Dictionary of TFs that are the same distance from a demand point m (alternatives)
+# TFd_sm = {}
+# # For each scenario s in S
+# for s in S:
+#     multiples = []
+#     # For each demand point m in M_s[s]
+#     for m in M_s[s]:
+#         # Find the closest distance
+#         closest_distance = 1000000
+#         # For each TF i in I_m[m]
+#         for i in TF_m[m]:
+#             if delta_im[(i,m)] < closest_distance:
+#                 closest_distance = delta_im[(i,m)]
+#         # Find the TFs that are that distance away
+#         closest_TFs = []
+#         # For each TF i in I_m[m]
+#         for i in TF_m[m]:
+#             if delta_im[(i,m)] == closest_distance:
+#                 closest_TFs.append(i)
+#         # Add to dictionary
+#         TFd_sm[(s,m)] = closest_TFs
+#         if len(closest_TFs) > 1:
+#             multiples.append(m)
+#     Md_s[s] = multiples
 
-# Kd_si # The remaining capacity of TF i after assigning the demand points to this TF
-# Need to know which demands points were assigned to which TF
-# for example Kd_si = {1: 45, 2, 62} where 1 and 2 are TFs and 46 62 are there respective remaining capacities
+# # Kd_si # The remaining capacity of TF i after assigning the demand points to this TF
+# # Need to know which demands points were assigned to which TF
+# # for example Kd_si = {1: 45, 2, 62} where 1 and 2 are TFs and 46 62 are there respective remaining capacities
 
-# Dictionary of TFs and their remaining capacities (given that the previous demand points are assigned)
-Kd_si = {}
-for s in S:
-    for m in M_s[s]:
-        for i in TF:
-            #if demand point m is assigned to TF i
-            if X[i,m].x > 0.9:
-                #then sum up the demand at that TF
-                demand_at_TF[i] = sum(D_sml[(s,m,l)] for l in L)
-                #find remaining demand
-                remaining_demand = []
-                amount = KT_i[i] - demand_at_TF[i]
-                remaining_demand.append(amount)
-            # Add to dictionary
-            KD_si[(s,i)] = remaining_demand
+# # Dictionary of TFs and their remaining capacities (given that the previous demand points are assigned)
+# Kd_si = {}
+# for s in S:
+#     for m in M_s[s]:
+#         for i in TF:
+#             #if demand point m is assigned to TF i
+#             if X[i,m].x > 0.9:
+#                 #then sum up the demand at that TF
+#                 demand_at_TF[i] = sum(D_sml[(s,m,l)] for l in L)
+#                 #find remaining demand
+#                 remaining_demand = []
+#                 amount = KT_i[i] - demand_at_TF[i]
+#                 remaining_demand.append(amount)
+#             # Add to dictionary
+#             KD_si[(s,i)] = remaining_demand
     
 
-ReducedCSP = Model("CheckForAlternative")
+# ReducedCSP = Model("CheckForAlternative")
 
-X = {(i,m): ReducedCSP.addVar(vtype=GRB.BINARY) for i in TF for m in M_s[s]}
+# X = {(i,m): ReducedCSP.addVar(vtype=GRB.BINARY) for i in TF for m in M_s[s]}
 
-#No objective 
+# #No objective 
 
-#Constraints
-Constraint24 = {m:
-        ReducedCSP.addConstr(quicksum(X[i,m] for i in TFd_sm) == 1) for m in Md_s[s]}
+# #Constraints
+# Constraint24 = {m:
+#         ReducedCSP.addConstr(quicksum(X[i,m] for i in TFd_sm) == 1) for m in Md_s[s]}
 
-Constraint25 = {i:
-            ReducedCSP.addConstr(quicksum(u_l[l]*D_sml[(s,m,l)]*X[i,m] for m in Md_s[s] for l in L) 
-            <= Kd_si[s][i]) 
-            for i in TFd_sm}
+# Constraint25 = {i:
+#             ReducedCSP.addConstr(quicksum(u_l[l]*D_sml[(s,m,l)]*X[i,m] for m in Md_s[s] for l in L) 
+#             <= Kd_si[s][i]) 
+#             for i in TFd_sm}
 
-# ReducedCSP.optimize()
+# # ReducedCSP.optimize()
 
 
-##########################
-#### FDSP is seperable over scenarios and item types 
-##########################
-FDSP = Model("Flow Distribution Sub-Problem")
+# ##########################
+# #### FDSP is seperable over scenarios and item types 
+# ##########################
+# FDSP = Model("Flow Distribution Sub-Problem")
 
-#Define a D_Bar
-D_bar = {(s,i,l): quicksum(D_sml[(s,m,l)]*X[i,s,m].x for m in M_s[s])
-         for s in S for i in TF for l in L}
+# #Define a D_Bar
+# D_bar = {(s,i,l): quicksum(D_sml[(s,m,l)]*X[i,s,m].x for m in M_s[s])
+#          for s in S for i in TF for l in L}
 
-#Variables
-# amount of item l transported from PJ j to TF i under scenario s
-F = {(j,i,l,s): FDSP.addVar() for s in S for j in PF for i in TF for l in L}
+# #Variables
+# # amount of item l transported from PJ j to TF i under scenario s
+# F = {(j,i,l,s): FDSP.addVar() for s in S for j in PF for i in TF for l in L}
 
-#No Objective
+# #No Objective
 
-#Constraint29
-FlowGreaterThanSCW = {(i,k):
-                     FDSP.addConstr(quicksum(F[j,i,l,s] for j in PF[k,i]) >= (r[k,l] * D_bar[(s,i,l)]))
-                     for i in TF for k in K}
+# #Constraint29
+# FlowGreaterThanSCW = {(i,k):
+#                      FDSP.addConstr(quicksum(F[j,i,l,s] for j in PF[k,i]) >= (r[k,l] * D_bar[(s,i,l)]))
+#                      for i in TF for k in K}
 
-#Constraint30
-FlowLessThanInventory = {j:
-                         FDSP.addConstr(quicksum(F[j,i,l,s] for i in TF)<= quicksum(I[j,l].x for l in L))
-    for j in PF}
+# #Constraint30
+# FlowLessThanInventory = {j:
+#                          FDSP.addConstr(quicksum(F[j,i,l,s] for i in TF)<= quicksum(I[j,l].x for l in L))
+#     for j in PF}
 
-#FDSP.optimize()
+# #FDSP.optimize()
 
-############################
-#If a feasible solution of the LIPMP is also feasible for CSP and FDSP sub-problems, it is also feasible for the original CLFDIP Problem 
-############################
+# ############################
+# #If a feasible solution of the LIPMP is also feasible for CSP and FDSP sub-problems, it is also feasible for the original CLFDIP Problem 
+# ############################
     
-# #Benders Loop 
-# for k in range(1):
+# # #Benders Loop 
+# # for k in range(1):
+# #     LIPMP.optimize()
+# #     CalcObj = 0
+# #     for s in S:########
+# #         for i in P[1]: ###########
+# #             if XXXXXX: ##CSP Infeasible
+# #                 LIPMP.addConstr(quicksum(Y[s,p] for p in P2)>= Y[s,i] for s in Sd for i in P1[s])
+# #                     if XXXXX ##FDSP infeasible 
+# #                         LIPMP.addConstr(quicksum(FlowLessThanInventory[j].pi * I[j,l] for j in PF) +\ 
+# #                                         quicksum(FlowGreaterThanSW[i,k].pi * r[k,l]*D[i,l] * Y[s,i] for i in TF for k in K) <=0)
+# #                         #####Should be \theta[r,j], but how would represent the extreme value here
+# # ###############################
+
+# #Define S_d P1 P2 for ReducedCSP
+# S_d #Set of scenarios which infeasible clusters are found 
+
+# P1 #Set of TF whose capacity is violated under scenario s in S_d 
+# P2 #Set of previously closed TF locations which are closer to at least one demand point assigned to TF i in scenario S in S_d 
+
+# #This loop is based on the Algorithm Flow Chart 
+# for kk in range(10):
 #     LIPMP.optimize()
-#     CalcObj = 0
-#     for s in S:########
-#         for i in P[1]: ###########
-#             if XXXXXX: ##CSP Infeasible
-#                 LIPMP.addConstr(quicksum(Y[s,p] for p in P2)>= Y[s,i] for s in Sd for i in P1[s])
-#                     if XXXXX ##FDSP infeasible 
-#                         LIPMP.addConstr(quicksum(FlowLessThanInventory[j].pi * I[j,l] for j in PF) +\ 
-#                                         quicksum(FlowGreaterThanSW[i,k].pi * r[k,l]*D[i,l] * Y[s,i] for i in TF for k in K) <=0)
-#                         #####Should be \theta[r,j], but how would represent the extreme value here
-# ###############################
-
-#Define S_d P1 P2 for ReducedCSP
-S_d #Set of scenarios which infeasible clusters are found 
-
-P1 #Set of TF whose capacity is violated under scenario s in S_d 
-P2 #Set of previously closed TF locations which are closer to at least one demand point assigned to TF i in scenario S in S_d 
-
-#This loop is based on the Algorithm Flow Chart 
-for kk in range(10):
-    LIPMP.optimize()
-    CutsAdded = 0 
-    for s in S:
+#     CutsAdded = 0 
+#     for s in S:
         
         
         
-        # define subproblem here
+#         # define subproblem here
         
         
         
         
-        CSP.optimize()
+#         CSP.optimize()
         
         
         
         
-        if CSP.status == GRB.INFEASIBLE:
+#         if CSP.status == GRB.INFEASIBLE:
             
-            # Make the set
+#             # Make the set
             
             
             
-            # Define reduced here
+#             # Define reduced here
             
             
-            ReducedCSP.optimize()
-            if ReducedCSP.status == GRB.INFEASIBLE:
-                #LIPMP.addConstr(quicksum(Y[p,s] for p in P2)>= Y[i,s] for s in S_d for i in P1[s]) #LBBD Cut 
-                CutsAdded +=1
+#             ReducedCSP.optimize()
+#             if ReducedCSP.status == GRB.INFEASIBLE:
+#                 #LIPMP.addConstr(quicksum(Y[p,s] for p in P2)>= Y[i,s] for s in S_d for i in P1[s]) #LBBD Cut 
+#                 CutsAdded +=1
                 
-            else:
+#             else:
                 
-                # third ...
-                for l in L:
-                    FDSP.optimize()
-                    if FDSP.status == GRB.INFEASIBLE:
-                        #LIPMP.addConstr() ## Duality cut (UP)
-                        CutsAdded +=1
-    if CutsAdded == 0:
-        break
+#                 # third ...
+#                 for l in L:
+#                     FDSP.optimize()
+#                     if FDSP.status == GRB.INFEASIBLE:
+#                         #LIPMP.addConstr() ## Duality cut (UP)
+#                         CutsAdded +=1
+#     if CutsAdded == 0:
+#         break
     
 
 
